4f2e1027f5a913eb540c3d6695648270b9605f02
dilipchoudhary14 <75087156+dilipchoudhary14@users.noreply.github.com>
2021-04-06 17:15:09 +0530

    Add files via upload

---
+++ goal4.py
@@ -0,0 +1,25 @@
+import random
+
+numbers=int(input("enter size of unique no-"))
+l=[]
+for i in range(numbers):
+    num=int(input("enter no-"))
+    l.append(num)
+
+print(l)
+
+def pick(l,numbers):
+    while(numbers>0):
+        randno=random.choice(l[0:numbers])
+        ind=l.index(randno)
+        temp=l[ind]
+        l[ind]=l[numbers-1]
+        l[numbers-1]=temp
+        print(randno)
+        
+       
+        numbers=numbers-1    
+
+
+
+pick(l,numbers)
